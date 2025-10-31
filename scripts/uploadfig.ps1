182f436bccc1e421aa0be96dca18352fb3d376ef
Thomas Niccolo Reyes <niccoreyes@gmail.com>
2025-10-31 12:51:43 +0800

    TEST: Terminology update - new script that post processes gofsh files

--- scripts/uploadfig.ps1
+++ scripts/uploadfig.ps1
@@ -1,13 +1,17 @@
 UploadFIG `
 -d "https://tx.fhirlab.net/fhir" `
 -s "https://build.fhir.org/ig/UPM-NTHC/PH-RoadSafetyIG/branches/35-add-examples-for-each-profile/package.r4.tgz" `
--r "StructureDefinition|Questionnaire|SearchParameter|StructureMap|Library" `
+-ip "example.fhir.ph.core|current" `
+-r StructureDefinition `
+-r Questionnaire `
+-r SearchParameter `
+-r StructureMap `
+-r Library `
+--includeReferencedDependencies
 # -ets "https://tx.fhirlab.net/fhir"
-#  -ip "example.fhir.ph.core|current" `
 #  -reg "https://cdr.fhir.net/fhir/" `
  #  -r StructureDefinition `
  #  -s "https://build.fhir.org/ig/UP-Manila-SILab/ph-core/package.r4.tgz" `
-#  --includeReferencedDependencies `
 
 # # Paths to your IG and ph-core package
 # $roadSafetyIG = "https://upm-nthc.github.io/PH-RoadSafetyIG/package.r4.tgz"
