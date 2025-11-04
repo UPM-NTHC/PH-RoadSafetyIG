ad35a96149d582e9b3ce52fd5995cf808c6c1480
Thomas Niccolo Reyes <niccoreyes@gmail.com>
2025-11-04 13:15:59 +0800

    feat(scripts): add upload-examples script for FHIR resource upload
    Added a new script `upload-examples.ts` to automate the process of uploading FHIR resources to a specified server. This script reads JSON files from the `fsh-generated/resources` directory, filters out excluded resource types, and uploads them in a specified order to handle dependencies. The script is integrated into the project via a new npm script command in `package.json`. This enhancement streamlines the deployment of FHIR resources, improving efficiency and reducing manual errors.

--- scripts/uploadfig.ps1
+++ scripts/uploadfig.ps1
@@ -1,6 +1,6 @@
 UploadFIG `
 -d "https://tx.fhirlab.net/fhir" `
--s "https://build.fhir.org/ig/UPM-NTHC/PH-RoadSafetyIG/branches/35-add-examples-for-each-profile/package.r4.tgz" `
+-s "https://build.fhir.org/ig/UPM-NTHC/PH-RoadSafetyIG/package.r4.tgz" `
 -ip "example.fhir.ph.core|current" `
 -r StructureDefinition `
 -r Questionnaire `
@@ -8,6 +8,19 @@ UploadFIG `
 -r StructureMap `
 -r Library `
 --includeReferencedDependencies
+
+UploadFIG `
+-d "https://cdr.fhirlab.net/fhir" `
+-s "https://build.fhir.org/ig/UPM-NTHC/PH-RoadSafetyIG/package.r4.tgz" `
+-ip "example.fhir.ph.core|current" `
+-r StructureDefinition `
+-r Questionnaire `
+-r SearchParameter `
+-r StructureMap `
+-r Library `
+--includeExamples `
+--includeReferencedDependencies
+
 # -ets "https://tx.fhirlab.net/fhir"
 #  -reg "https://cdr.fhir.net/fhir/" `
  #  -r StructureDefinition `
